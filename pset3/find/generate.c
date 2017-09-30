5ee1c1699e1a03dda330ea51dc5db936b9583e76
Vivek Vishwakarma <viveksallu156@gmail.com>
2017-10-01 03:48:27 +0530

    Add files via upload

---
+++ pset3/find/generate.c
@@ -0,0 +1,52 @@
+/**
+ * generate.c
+ *
+ * Generates pseudorandom numbers in [0,MAX), one per line.
+ *
+ * Usage: generate n [s]
+ *
+ * where n is number of pseudorandom numbers to print
+ * and s is an optional seed
+ */
+ 
+#define _XOPEN_SOURCE
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// upper limit on range of integers that can be generated
+#define LIMIT 65536
+
+int main(int argc, string argv[])
+{
+    // argc counts the number of command line arguments given by user must be 2 or 3 
+    if (argc != 2 && argc != 3)
+    {
+        printf("Usage: ./generate n [s]\n");
+        return 1;
+    }
+
+    // converts the string to integer
+    int n = atoi(argv[1]);
+
+    // argc counts the command line arguments if 3 then generate srand48
+    if (argc == 3)
+    {
+        srand48((long) atoi(argv[2]));
+    }
+    else
+    {
+        srand48((long) time(NULL));
+    }
+
+    // iterate over to print the random numbers
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", (int) (drand48() * LIMIT));
+    }
+
+    // success
+    return 0;
+}
